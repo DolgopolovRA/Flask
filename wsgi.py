6951d052bb34fcac3c10df21c2432be68d1c611d
DolgopolovRA <ra.dolgopolov@yandex.ru>
2023-03-15 10:04:33 +0300

    dz_5

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,6 @@
 from blog.models import User
 from blog.app import app, db
+import os
 
 
 if __name__ == "__main__":
@@ -9,26 +10,16 @@ if __name__ == "__main__":
     )
 
 
-@app.cli.command("init-db")
-def init_db():
+@app.cli.command("create-admin")
+def create_admin():
     """
     Run in your terminal:
-    flask init-db
+    ➜ flask create-admin
+    > created admin: <User #1 'admin'>
     """
-    db.create_all()
-    print("done!")
-
-
-@app.cli.command("create-users")
-def create_users():
-    """
-    Run in your terminal:
-    flask create-users
-    > done! created users: <User #1 'admin'> <User #2 'james'>
-    """
-    admin = User(username="admin", is_staff=True, is_active=True)
-    james = User(username="james", is_staff=False, is_active=True)
+    from blog.models import User
+    admin = User(username="admin", is_staff=True)
+    admin.password = os.environ.get("ADMIN_PASSWORD") or "adminpass"
     db.session.add(admin)
-    db.session.add(james)
     db.session.commit()
-    print("done! created users:", admin, james)
+    print("created admin:", admin)
